454de0c5700c88a3b4001cb12e871d6c584192d6
elina <elina.faber@t-online.de>
2020-07-14 17:15:43 +0200

    about page

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class About extends React.Component {
+	render() {
+		return (
+			<div>
+				<p>Please choose a repository from the list below.</p>
+			</div>
+		);
+	}
+}
+
+export default About;
