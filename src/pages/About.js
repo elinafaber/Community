a7811e7dae58529eff23aa3ccfde856a9050f365
elina <elina.faber@t-online.de>
2020-07-16 13:30:27 +0200

    side comments library integration

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,81 @@
 import React from 'react';
+import classNames from 'classnames';
+import { SectionTilesProps } from '../utils/SectionProps';
+import SectionHeader from '../components/sections/partials/SectionHeader';
 
-class About extends React.Component {
-	render() {
-		return (
-			<div>
-				<p>Please choose a repository from the list below.</p>
-			</div>
-		);
-	}
+const propTypes = {
+  ...SectionTilesProps.types
 }
 
-export default About;
+const defaultProps = {
+  ...SectionTilesProps.defaults
+}
+
+const Testimonial = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+
+  const outerClasses = classNames(
+    'testimonial section',
+    topOuterDivider && 'has-top-divider',
+    bottomOuterDivider && 'has-bottom-divider',
+    hasBgColor && 'has-bg-color',
+    invertColor && 'invert-color',
+    className
+  );
+
+  const innerClasses = classNames(
+    'testimonial-inner section-inner',
+    topDivider && 'has-top-divider',
+    bottomDivider && 'has-bottom-divider'
+  );
+
+  const tilesClasses = classNames(
+    'tiles-wrap',
+    pushLeft && 'push-left'
+  );
+
+  const sectionHeader = {
+    title: 'Case study: The vulnerability mode',
+    paragraph: 'Vitae aliquet nec ullamcorper sit amet risus nullam eget felis semper quis lectus nulla at volutpat diam ut venenatis tellus—in ornare.'
+  };
+
+  return (
+    <section
+      {...props}
+      className={outerClasses}
+    >
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+		  <div id="commentable-container" class="container commentable-container">
+			<div id="commentable-area">
+				<p data-section-id="1" class="commentable-section">
+				This is a section that can be commented on.
+				</p>
+				<p data-section-id="2" class="commentable-section">
+				This is a another section that can be commented on.
+				</p>
+				<p data-section-id="3" class="commentable-section">
+				This is yet another section that can be commented on.
+				</p>
+			</div>
+   		 </div>
+		</div>
+		</div>
+    </section>
+  );
+}
+
+Testimonial.propTypes = propTypes;
+Testimonial.defaultProps = defaultProps;
+
+export default Testimonial;
